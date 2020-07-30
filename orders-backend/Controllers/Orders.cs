bfa08d32284269663e29ed62d8e64a2ca36a6a8e
jakub.pawlowski <jakub.pawlowski@live.com>
2020-07-30 10:58:55 +0200

    initial commit

---
+++ orders-backend/Controllers/Orders.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace orders_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ILogger<OrdersController> _logger;
+        private static HttpClient warehouseClient = new HttpClient();
+
+        public OrdersController(ILogger<OrdersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult> Order([FromBody] Order order)
+        {
+            order.Id = Guid.NewGuid();
+
+            // _logger.LogDebug($"received a new order: {order}");
+            _logger.LogDebug("received a new order: {order}", order);
+
+            var response = await warehouseClient.GetAsync("https://localhost:5002/items/");
+            
+            return Ok(order);
+        }
+    }
+
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public List<string> Items {get;set;}
+    }
+}
---
+++ warehouse/Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace warehouse.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ILogger<ItemsController> _logger;
+
+        public ItemsController(ILogger<ItemsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Get()
+        {
+            foreach(var header in Request.Headers) {
+                _logger.LogDebug($"{header.Key}: {header.Value}");
+            }
+            _logger.LogDebug("returning available items");
+            return Ok(new { avaliable = 4 });
+        }
+    }
+}
