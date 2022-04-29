c26c7cdc00897d18e54eb2c60b62e14b7a04f461
jakub.pawlowski <jakub.pawlowski@live.com>
2022-04-29 10:57:55 +0200

    migrate warehouse

---
+++ orders/Orders.cs
@@ -0,0 +1,77 @@
+using System.Diagnostics;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var config = app.Configuration;
+
+var warehouseClient = new HttpClient();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/orders/", async (Order order) =>
+{
+    order.Id = Guid.NewGuid();
+
+    app.Logger.LogDebug("received a new order: {order}", order);
+
+    var response = await warehouseClient.GetAsync("https://localhost:5002/items/");
+    System.Console.WriteLine(config["EventGrid:Hostname"]);
+
+    using var eventGrid = new EventGridClient(new TopicCredentials(config["EventGrid:Key"]));
+
+    var activity = Activity.Current!;
+
+    await eventGrid.PublishEventsAsync(config["EventGrid:Hostname"], new[] {
+        new EventGridEvent {
+            Id = Guid.NewGuid().ToString(),
+            Topic = "orders",
+            Data = JObject.FromObject(new {
+                Order = order,
+                traceparent = activity.TraceParent(),
+                Activity = new {
+                    activity.RootId,
+                    activity.Id,
+                    activity.ParentId,
+                    activity.ParentSpanId,
+                    activity.SpanId,
+                    activity.TraceId,
+                }
+            }),
+            EventType = "OrderAccepted",
+            Subject = $"orders/{order.Id}",
+            DataVersion = "1.0.1"
+        }
+    });
+
+    return order;
+});
+
+app.Run();
+
+
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public List<string> Items { get; set; }
+}
+
+public static class ActivityExtensions
+{
+    public static string? TraceParent(this Activity activity)
+    {
+        if (activity?.SpanId == null || activity?.Id == null) return null;
+
+        var nextSpanId = ActivitySpanId.CreateRandom().ToHexString();
+        activity.AddBaggage("next_spanId", nextSpanId);
+
+        var currentSpanId = activity.SpanId.ToHexString();
+        var traceparent = activity.Id.Replace(currentSpanId, nextSpanId);
+
+        return traceparent;
+    }
+}
---
+++ warehouse/Warehouse.cs
@@ -0,0 +1,110 @@
+using System.Diagnostics;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json.Linq;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var _logger = app.Logger;
+var config = app.Configuration;
+
+app.MapGet("/items", async (HttpRequest request) =>
+{
+    foreach (var header in request.Headers)
+    {
+        _logger.LogDebug($"{header.Key}: {header.Value}");
+    }
+    _logger.LogDebug("returning available items");
+
+    var itemsAvailable = 4;
+
+    using var eventGrid = new EventGridClient(new TopicCredentials(config["EventGrid:Key"]));
+
+    var activity = Activity.Current!;
+
+    await eventGrid.PublishEventsAsync(config["EventGrid:Hostname"], new List<EventGridEvent>()
+        {
+            new EventGridEvent()
+            {
+                Id = Guid.NewGuid().ToString(),
+                    Topic = "items",
+                    Data = JObject.FromObject(new
+                    {
+                        traceparent = activity.TraceParent(),
+                        ItemsAvailable = itemsAvailable,
+                        Activity = new
+                        {
+                            activity.RootId,
+                            activity.Id,
+                            activity.ParentId,
+                            activity.ParentSpanId,
+                            activity.SpanId,
+                            activity.TraceId,
+                        }
+                    }),
+                    EventType = "WarehouseDepleted",
+                    Subject = $"warehouse",
+                    DataVersion = "1.0.1"
+            }
+        });
+
+    return new { avaliable = itemsAvailable };
+});
+
+app.Run();
+
+
+public class CloudRoleNameTelemetryInitializer : ITelemetryInitializer
+{
+    public readonly string roleName;
+    public CloudRoleNameTelemetryInitializer(string roleName)
+    {
+        this.roleName = roleName;
+    }
+    public void Initialize(ITelemetry telemetry)
+    {
+        // set custom role name here
+        telemetry.Context.Cloud.RoleName = this.roleName;
+    }
+}
+
+public class EventGridDependencyInitializer : ITelemetryInitializer
+{
+    public EventGridDependencyInitializer()
+    {
+    }
+    public void Initialize(ITelemetry telemetry)
+    {
+        if (telemetry is DependencyTelemetry dependency)
+        {
+            var activity = Activity.Current!;
+            var id = activity.GetBaggageItem("next_spanId");
+            if (!string.IsNullOrEmpty(id))
+            {
+                dependency.Id = id;
+                dependency.Type = "Azure Service Bus";
+            }
+        }
+
+    }
+}
+
+public static class ActivityExtensions
+{
+    public static string? TraceParent(this Activity activity)
+    {
+        if (activity?.SpanId == null || activity?.Id == null) return null;
+
+        var nextSpanId = ActivitySpanId.CreateRandom().ToHexString();
+        activity.AddBaggage("next_spanId", nextSpanId);
+
+        var currentSpanId = activity.SpanId.ToHexString();
+        var traceparent = activity.Id.Replace(currentSpanId, nextSpanId);
+
+        return traceparent;
+    }
+}
